4582dc1d97596f821b2f4f8a881362268f911bd6
cmdscm <134817738+cmdscm@users.noreply.github.com>
2023-10-16 13:04:29 +0800

    Update click.js

--- int/click.js
+++ int/click.js
@@ -1,10 +1,8 @@
 /* search */
-var searchWayNum = 1
-var searchSec = document.querySelector("#search")
-var searchWay = document.getElementById("search-way");
-var searchSub = document.getElementById("search-submit");
-var searchImg = document.getElementById("search-img");
+var searchWayNum = 1;
 function searchWays(){
+    let searchFom = document.getElementById("search-form");
+    let searchPut = document.getElementById("search-input");
     let searchImg = document.getElementById("search-img");
     let searchSub = document.getElementById("search-submit");
     let searchSec = document.querySelector("#search");
@@ -14,11 +12,17 @@ function searchWays(){
         searchSec.classList.add("search-ts");
         searchWay.innerHTML = "切换至百度搜索";
         searchSub.value = "360搜索";
+        searchImg.src = "./360.png";
+        searchPut.name = "q";
+        searchFom.action = "https://www.so.com/s";
         searchWayNum = 2;
     }else{
         searchSec.classList.replace("search-ts","search-bd");
         searchWay.innerHTML = "切换至360搜索";
         searchSub.value = "百度一下";
+        searchImg.src = "./baidu.png";
+        searchPut.name = "wd";
+        searchFom.action = "https://www.baidu.com/s"
         searchWayNum = 1;
     }; 
 };
